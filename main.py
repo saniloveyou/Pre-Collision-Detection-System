a2acad5bc3bf52dca141e8113a91cf55a9dfa08b
Jacky-lim-data-analyst <84822819+Jacky-lim-data-analyst@users.noreply.github.com>
2023-02-23 20:19:16 +0800

    mobilenet_v3
    Files: 
    1. video samples
    2. model configuration, model weights, coco annotations
    3. 2 source code files

---
+++ main.py
@@ -0,0 +1,17 @@
+from objdet_1 import *
+
+import os
+
+def main():
+    videoPath = "videos/video_street.mp4"
+
+    configPath = os.path.join("model_data", "ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt")
+    modelPath = os.path.join("model_data", "frozen_inference_graph.pb")
+
+    classesPath = os.path.join("model_data", "coco.names")
+
+    detector = Detector(videoPath, configPath, modelPath, classesPath)
+    detector.onVideo()
+
+if __name__ == "__main__":
+    main()
